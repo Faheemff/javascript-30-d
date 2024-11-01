fe741004836395dbcfbef53531dcdbb5312437f3
Faheemff <mfaheemweb@gmail.com>
2024-11-01 09:34:39 -0700

    add more

--- 4-script.js
+++ 4-script.js
@@ -1,1 +1,58 @@
-con
+{
+    let carName = 'Toyata';
+};
+{                                 // That called Block Scope in javascript.js
+    let carName = 'Toyata';
+};
+{
+    let carName = 'Toyata';
+
+    console.log(carName);
+};
+
+
+// Function scope in javascript.js;
+
+function man() {
+    var name = 'Hello';          // thaat called function scope in javascript
+};
+
+man();
+
+var x = 10;
+
+// let x;
+// x = 20;
+
+// console.log(x);
+
+
+
+
+let Hel = 40;
+
+function maan() {
+    let hel  = 10;
+    console.log(Hel);
+    console.log(hel);
+};
+
+
+maan();
+
+console.log(Hel);
+
+
+
+
+///  now move on Promises.
+
+
+let promises = new Promise((res, rej)=> {
+    alert("Hello")
+    res(56)
+});
+
+
+
+console.log(promises);
