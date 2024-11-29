8dab147ea75ec6a4bc3e5f6ff74a24786a446283
Faheemff <mfaheemweb@gmail.com>
2024-11-29 22:05:07 +0500

    add more files

--- 6-script.js
+++
@@ -1,8 +0,0 @@
-let functio =  ()=> {
-    async function careteApi(params) {
-        let response = await fetch('https://api.example.com/data');
-        let data = await response.json();
-        console.log(data);
-        res(data);
-    }
-}
