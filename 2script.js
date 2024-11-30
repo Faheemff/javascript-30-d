f66c6319a9664e2e9583cdbf597c7cccc8cef208
Faheemff <mfaheemweb@gmail.com>
2024-11-30 22:09:10 +0500

    add more files

--- 2script.js
+++ 2script.js
@@ -1,1 +1,8 @@
 const cars = ["Saab", "Volvo", "BMW"];
+const cars2 = ["Saab", "Volvo", "BMW"];
+const cars3 = ["Saab", "Volvo", "BMW"];
+const cars4 = ["Saab", "Volvo", "BMW"];
+const cars5 = ["Saab", "Volvo", "BMW"];
+const cars6= ["Saab", "Volvo", "BMW"];
+
+const cars7 = ["Saab", "Volvo", "BMW"];
