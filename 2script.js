3fede5dcfca9f957d36927a6ec5d2b1b25e7e999
Faheemff <mfaheemweb@gmail.com>
2024-12-01 10:16:54 +0500

    add more files

--- 2script.js
+++ 2script.js
@@ -1,8 +1,3 @@
-const cars = ["Saab", "Volvo", "BMW"];
-const cars2 = ["Saab", "Volvo", "BMW"];
-const cars3 = ["Saab", "Volvo", "BMW"];
-const cars4 = ["Saab", "Volvo", "BMW"];
-const cars5 = ["Saab", "Volvo", "BMW"];
-const cars6= ["Saab", "Volvo", "BMW"];
-
-const cars7 = ["Saab", "Volvo", "BMW"];
+let cars = ["A", "B", "C", "D"];
+cars.toString();
+console.log(cars);
