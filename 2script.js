d5ac0f2c0cba263b8545ddf0b50f13078b74bd78
Faheemff <mfaheemweb@gmail.com>
2024-12-01 22:57:36 +0500

    add more files

--- 2script.js
+++ 2script.js
@@ -1,3 +1,15 @@
-let cars = ["A", "B", "C", "D"];
-let car = cars.toString();
-console.log(car);
+// from method in Array
+
+
+let text = "hello";
+let array = Array.from(text);
+console.log(array);
+
+
+// from method ka use kesi bi chez ko ya string ya iterals ko arrya bana ka leya keya jata hai ya is element kr kr ek element ko change kr ka array mai tabdail kr da ta hai
+
+
+// Array.of method in array
+
+let array1 = Array.of(1,2,3,4,5,6);
+console.log(array1);
