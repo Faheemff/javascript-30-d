71958f0e51a67be4e369662fb19476dc5306b413
Faheemff <mfaheemweb@gmail.com>
2024-12-03 00:15:46 +0500

    add more files

--- 2script.js
+++ 2script.js
@@ -4,8 +4,6 @@
 let text = "hello";
 let array = Array.from(text);
 console.log(array);
-
-
 // from method ka use kesi bi chez ko ya string ya iterals ko arrya bana ka leya keya jata hai ya is element kr kr ek element ko change kr ka array mai tabdail kr da ta hai
 
 
@@ -13,3 +11,15 @@ console.log(array);
 
 let array1 = Array.of(1,2,3,4,5,6);
 console.log(array1);
+
+
+let ar = [1,2,3,4,5,6];
+localStorage.setItem
+
+// map method in array
+
+let numbers = [1,2,3,4,5];
+let doubledNumbers = numbers.map(function(number) {
+    return number * 2;
+});
+console.log(doubledNumbers);
