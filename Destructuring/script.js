68c7091f6818aa3efcb1ab77a054f0c4f0af55b5
Faheemff <mfaheemweb@gmail.com>
2024-12-05 22:00:30 +0500

    add

--- Destructuring/script.js
+++ Destructuring/script.js
@@ -22,3 +22,23 @@ console.log(second1);
 console.log(third1);
 
 
+// destructuring with object
+
+let obj = {name: 'John', age: 30};
+
+
+// bina destructuring
+
+let firstName = obj.name;
+let secondAge = obj.age;
+
+console.log(firstName);
+console.log(secondAge);
+
+
+// now destructuring
+
+let {name:name, age:age } = obj;
+console.log(name);
+// console.log(age);
+
