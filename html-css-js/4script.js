a2457ce8110315f695fae42513d5690a1c50ec47
Faheemff <mfaheemweb@gmail.com>
2024-12-05 21:05:46 +0500

    add

--- html-css-js/4script.js
+++ html-css-js/4script.js
@@ -25,5 +25,40 @@ console.log(newArr);
 
 
 // filter
+
+let arr3 = ['Alice', 'Bob', 'Charlie', 'David', 'Emma'];
+let newArray = arr3.filter((value)=> {
+    if(value.startsWith('A')) {
+        return value;
+    }
+})
 // destructuring
+
+const user = {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@example.com",
+    age: 28,
+    isActive: true,
+    roles : ["User", "Admin"],
+    preferences: {
+        theme: "dark",
+        language: "English",
+        notifications: true
+    },
+    loginHistory: [
+        { date: "2024-12-01", ipAddress: "192.168.1.1" },
+        { date: "2024-12-02", ipAddress: "192.168.1.2" }
+    ]
+};
+
+let  loginHistory  = user.loginHistory;
+console.log(loginHistory);
+
+
+let [_, second] = user.loginHistory;
+console.log(second);
+
+
+
 // spread and rest
