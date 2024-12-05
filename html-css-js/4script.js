97c0df0a6891508e85f4fbc7057b49ee3f9d291f
Faheemff <mfaheemweb@gmail.com>
2024-12-05 20:29:04 +0500

    add

--- html-css-js/4script.js
+++ html-css-js/4script.js
@@ -1,6 +1,29 @@
-let map = map();
+// foreach
 
-let arr = [1,2,3,4,5,6,7];
-arr.forEach(function () {
-    map.set(arr.indexOf(this), this);
-})
+
+let arr = ['Alice', 'Bob', 'Charlie', 'David', 'Emma'];
+
+arr.forEach((value)=> {
+    if(value === 'Alice') {
+
+    } else {
+        console.log(value);
+        
+    }
+});
+
+
+// map
+
+let arr2 = ['Alice', 'Bob', 'Charlie', 'David', 'Emma'];
+let newArr = arr2.map((value)=> {
+    return value + "g";
+});
+console.log(newArr);
+
+
+
+
+// filter
+// destructuring
+// spread and rest
