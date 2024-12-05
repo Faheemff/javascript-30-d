f36fb2d8ff6389ada44a096c5d5ed6178381ae6f
Faheemff <mfaheemweb@gmail.com>
2024-12-05 21:51:37 +0500

    add

---
+++ Destructuring/script.js
@@ -0,0 +1,24 @@
+// destructuring with array 
+
+let arr = ['a', 'b', 'c'];
+
+// bina destructuring
+
+let first = arr[0];
+let second = arr[1];
+let third = arr[2];
+
+console.log(first);
+console.log(second);
+console.log(third);
+
+
+// now destructuring
+
+let [first1, second1, third1] = arr;
+
+console.log(first1);
+console.log(second1);
+console.log(third1);
+
+
