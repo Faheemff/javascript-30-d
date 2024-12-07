50f3a62df255684f68f3da47570d7fa6d521f502
Faheemff <mfaheemweb@gmail.com>
2024-12-07 23:07:11 +0500

    add

---
+++ functionss/script.js
@@ -0,0 +1,24 @@
+// Basic Function Create
+
+let strr = "Create Function";
+
+function CreateFunction() {
+    return console.log(strr);
+    
+};
+
+CreateFunction();
+
+
+
+let a = 10;
+let b = 20;
+
+//  That is   | parameter 
+function sum(a,b){
+    return a + b;
+};
+
+// That is    | argument
+let total = sum(a,b);
+console.log(total);
