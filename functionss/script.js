151cb444b18a108008a21e4be4c4fb1d21c3f2a2
Faheemff <mfaheemweb@gmail.com>
2024-12-07 23:25:32 +0500

    add

--- functionss/script.js
+++ functionss/script.js
@@ -22,3 +22,23 @@ function sum(a,b){
 // That is    | argument
 let total = sum(a,b);
 console.log(total);
+
+
+
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
+
+
+
+function greet(name = "Guest") {
+    console.log("Hello, " + name + "!");
+}
+
+greet(); // Output: Hello, Guest!
+
